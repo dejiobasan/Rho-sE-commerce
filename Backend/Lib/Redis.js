2e6c5eb630be92dd28719345da225293e6a1b0fa
Ayodeji Obasan <dejiobasan02@gmail.com>
2025-08-28 20:47:57 +0100

    fixed redis errors

--- Backend/Lib/Redis.js
+++ Backend/Lib/Redis.js
@@ -1,14 +1,23 @@
 const Redis = require("ioredis");
 require("dotenv").config();
 
-
 // Create a new Redis instance
 const redis = new Redis(process.env.UPSTASH_REDIS_URL);
 
+redis.on("connect", () => {
+  console.log("Connected to Redis");
+});
+
+redis.on("error", (err) => {
+  console.error("Redis error:", err.message);
+});
+
+redis.on("close", () => {
+  console.warn("Redis connection closed");
+});
+
+redis.on("reconnecting", () => {
+  console.log("Reconnecting to Redis...");
+});
 
 module.exports = redis;
-
-
-
-
-
